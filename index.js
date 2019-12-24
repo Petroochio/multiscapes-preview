38575d5adeaa56b707b70944d36be1187cfd79be
Peter Gyory <ptgyory@gmail.com>
2019-12-24 16:53:40 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const http = require('http');
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 5000 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
+  ws.send('something');
+});
+console.log('ws go!');
+// TCP for thing
+var net = require('net');
+
+var s = net.createServer();
+s.listen(1337, '192.168.1.203');
+
+let sockets = [];
+
+s.on('connection', function(sock) {
+    console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
+    sockets.push(sock);
+
+    sock.on('data', function(data) {
+        // console.log('DATA ' + sock.remoteAddress + ': ' + data);
+        const dataString = '' + data; // hacky
+        // console.log(dataString[0]);
+        if (dataString[0] === 'c') {
+          const capVal = parseInt(dataString.split('-')[1]);
+          // console.log(capVal);
+          if (capVal > 2500) {
+            wss.clients.forEach(c => c.send('CAP_HIGH'));
+          } else {
+            wss.clients.forEach(c => c.send('CAP_LOW'));
+          }
+        }
+        // Write the data back to all the connected, the client will receive it as data from the server
+        // sockets.forEach(function(sock, index, array) {
+        //     sock.write(sock.remoteAddress + ':' + sock.remotePort + " said " + data + '\n');
+        // });
+    });
+});
+console.log('tcp go!');
