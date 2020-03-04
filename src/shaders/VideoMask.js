cf3c4806bb1e2c2438a23a2b0c6435e3ee8426d1
Peter Gyory <ptgyory@gmail.com>
2020-03-04 12:48:24 -0700

    clement's fancy mask:

--- src/shaders/VideoMask.js
+++ src/shaders/VideoMask.js
@@ -134,6 +134,12 @@ const matHoles = `
   void main() {
     float r0 = 0.0, r1 = 0.0, r2 = 0.0, r3 = 0.0, r4 = 0.0, r5 = 0.0, r6 = 0.0, r7 = 0.0, r8 = 0.0;
     float r9 = 0.0, r10 = 0.0, r11 = 0.0, r12 = 0.0, r13 = 0.0, r14 = 0.0, r15 = 0.0;
+
+    float threshold = 0.75;
+
+    float t0 = 0.0, t1 = 0.0, t2 = 0.0, t3 = 0.0, t4 = 0.0, t5 = 0.0, t6 = 0.0, t7 = 0.0, t8 = 0.0;
+    float t9 = 0.0, t10 = 0.0, t11 = 0.0, t12 = 0.0, t13 = 0.0, t14 = 0.0, t15 = 0.0;
+
     r0 = distance(vUv, mat_0.xy);
     r1 = distance(vUv, mat_1.xy);
     r2 = distance(vUv, mat_2.xy);
@@ -155,18 +161,88 @@ const matHoles = `
     r14 = distance(vUv, mat_14.xy);
     r15 = distance(vUv, mat_15.xy);
 
+    t0 = r0 / mat_0.z;
+    t1 = r1 / mat_1.z;
+    t2 = r2 / mat_2.z;
+
+    t3 = r3 / mat_3.z;
+    t4 = r4 / mat_4.z;
+    t5 = r5 / mat_5.z;
+
+    t6 = r6 / mat_6.z;
+    t7 = r7 / mat_7.z;
+    t8 = r8 / mat_8.z;
+
+    t9 = r9 / mat_9.z;
+    t10 = r10 / mat_10.z;
+    t11 = r11 / mat_11.z;
+
+    t12 = r12 / mat_12.z;
+    t13 = r13 / mat_13.z;
+    t14 = r14 / mat_14.z;
+    t15 = r15 / mat_15.z;
+
     vec4 color = vec4(0.0, 0.0, 0.0, 1.0);
-    if (r0 < mat_0.z || r1 < mat_1.z || r2 < mat_2.z) {
-      color = vec4(0.0, 0.0, 0.0, 0.0);
-    } else if (r3 < mat_3.z || r4 < mat_4.z || r5 < mat_5.z) {
-      color = vec4(0.0, 0.0, 0.0, 0.0);
-    } else if (r6 < mat_6.z || r7 < mat_7.z || r8 < mat_8.z) {
-      color = vec4(0.0, 0.0, 0.0, 0.0);
-    } else if (r9 < mat_9.z || r10 < mat_10.z || r11 < mat_11.z) {
-      color = vec4(1.0, 0.0, 0.0, 0.0);
-    } else if (r12 < mat_12.z || r13 < mat_13.z || r14 < mat_14.z || r15 < mat_15.z) {
-      color = vec4(0.0, 0.0, 0.0, 0.0);
+
+    if (t0 < threshold || t1 < threshold || t2 < threshold) {
+      color.a = 0;
+    } else if (t3 < threshold || t4 < threshold || t5 < threshold) {
+      color.a = 0;
+    } else if (t6 < threshold || t7 < threshold || t8 < threshold) {
+      color.a = 0;
+    } else if (t9 < threshold || t10 < threshold || t11 < threshold) {
+      color.a = 0;
+    } else if (t12 < threshold || t13 < threshold || t14 < threshold || t15 < threshold) {
+      color.a = 0;
     }
+
+    if (t0 >= threshold && t0 < 1) {
+      color.a = color.a + ((-threshold+t0)/(1-threshold))*((-threshold+t0)/(1-threshold));
+    } else if (t1 >= threshold && t1 < 1) {
+      color.a = color.a + ((-threshold+t1)/(1-threshold))*((-threshold+t1)/(1-threshold));
+    } else if (t2 >= threshold && t2 < 1) {
+      color.a = color.a + ((-threshold+t2)/(1-threshold))*((-threshold+t2)/(1-threshold));
+    } else if (t3 >= threshold && t3 < 1) {
+      color.a = color.a + ((-threshold+t3)/(1-threshold))*((-threshold+t3)/(1-threshold));
+    } else if (t4 >= threshold && t4 < 1) {
+      color.a = color.a + ((-threshold+t4)/(1-threshold))*((-threshold+t4)/(1-threshold));
+    } else if (t5 >= threshold && t5 < 1) {
+      color.a = color.a + ((-threshold+t5)/(1-threshold))*((-threshold+t5)/(1-threshold));
+    } else if (t6 >= threshold && t6 < 1) {
+      color.a = color.a + ((-threshold+t6)/(1-threshold))*((-threshold+t6)/(1-threshold));
+    } else if (t7 >= threshold && t7 < 1) {
+      color.a = color.a + ((-threshold+t7)/(1-threshold))*((-threshold+t7)/(1-threshold));
+    } else if (t8 >= threshold && t8 < 1) {
+      color.a = color.a + ((-threshold+t8)/(1-threshold))*((-threshold+t8)/(1-threshold));
+    } else if (t9 >= threshold && t9 < 1) {
+      color.a = color.a + ((-threshold+t9)/(1-threshold))*((-threshold+t9)/(1-threshold));
+    } else if (t10 >= threshold && t10 < 1) {
+      color.a = color.a + ((-threshold+t10)/(1-threshold))*((-threshold+t10)/(1-threshold));
+    } else if (t11 >= threshold && t11 < 1) {
+      color.a = color.a + ((-threshold+t11)/(1-threshold))*((-threshold+t11)/(1-threshold));
+    } else if (t12 >= threshold && t12 < 1) {
+      color.a = color.a + ((-threshold+t12)/(1-threshold))*((-threshold+t12)/(1-threshold));
+    } else if (t13 >= threshold && t13 < 1) {
+      color.a = color.a + ((-threshold+t13)/(1-threshold))*((-threshold+t13)/(1-threshold));
+    } else if (t14 >= threshold && t14 < 1) {
+      color.a = color.a + ((-threshold+t14)/(1-threshold))*((-threshold+t14)/(1-threshold));
+    } else if (t15 >= threshold && t15 < 1) {
+      color.a = color.a + ((-threshold+t15)/(1-threshold))*((-threshold+t15)/(1-threshold));
+    }
+
+    if (color.a > 1) color.a = 1;
+    
+    // if (r0 < mat_0.z || r1 < mat_1.z || r2 < mat_2.z) {
+    //   color = vec4(0.0, 0.0, 0.0, 0.0);
+    // } else if (r3 < mat_3.z || r4 < mat_4.z || r5 < mat_5.z) {
+    //   color = vec4(0.0, 0.0, 0.0, 0.0);
+    // } else if (r6 < mat_6.z || r7 < mat_7.z || r8 < mat_8.z) {
+    //   color = vec4(0.0, 0.0, 0.0, 0.0);
+    // } else if (r9 < mat_9.z || r10 < mat_10.z || r11 < mat_11.z) {
+    //   color = vec4(1.0, 0.0, 0.0, 0.0);
+    // } else if (r12 < mat_12.z || r13 < mat_13.z || r14 < mat_14.z || r15 < mat_15.z) {
+    //   color = vec4(0.0, 0.0, 0.0, 0.0);
+    // }
 
     gl_FragColor = color;
   }
